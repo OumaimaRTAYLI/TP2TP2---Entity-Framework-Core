6aba16370582cacc34e14a1e052dc03da09ac833
OumaimaRTAYLI <oumaima.rtayli@gmail.com>
2024-09-27 11:06:16 +0200

    init

---
+++ TP2console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP2console.Models.EntityFramework;
+
+namespace TP2console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var ctx = new FilmsDbContext())
+        {
+            //Désactivation du tracking => Aucun chanegement dans la base ne sera effectué
+            //ctx.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            //Requête SELECT
+            Film titanic = ctx.Films.AsNoTracking().First(f => f.Nom.Contains("Titanic"));
+            
+            //Modification de l'entité (dans le contexte seulement)
+            titanic.Description = "Un bateau a échoué. Date : " + DateTime.Now;
+            
+            //Sauvegarde du ctx => Application de la modification dans la BD
+            int nbchanges = ctx.SaveChanges();
+            
+            Console.WriteLine("Nombre d'enregistrement modifiés ou ajoutés : " + nbchanges);
+
+            
+        }
+    }
+}
